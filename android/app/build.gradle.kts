2dc43b1c6c99a07a84096d55c76c099497976031
Lee j c 59 <eotjddls902@gmail.com>
2025-08-22 18:46:47 +0900

    안드로이드 패키지명 변경 key 관련 해서 바꾸고 연결 시켜줌
    현재 로그인 회원가입시 다음으로 안넘어감
    그런데 회원가입은 됨

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,18 +1,8 @@
-import java.util.Properties
-import java.io.FileInputStream
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
-}
-
-val keystoreProps = Properties().apply {
-    val f = rootProject.file("key.properties")
-    if (f.exists()) {
-        load(FileInputStream(f))
-    }
 }
 
 android {
@@ -33,30 +23,18 @@ android {
     }
     kotlinOptions { jvmTarget = "17" }
 
-    // ✅ 항상 'release' 서명 구성을 "생성"한다
     signingConfigs {
         create("release") {
-            // key.properties 가 있을 때만 값 채우기
-            val storePath = keystoreProps.getProperty("storeFile")
-            if (!storePath.isNullOrBlank()) {
-                // 루트 기준 경로로 해석
-                storeFile = rootProject.file(storePath)
-                storePassword = keystoreProps.getProperty("storePassword")
-                keyAlias = keystoreProps.getProperty("keyAlias")
-                keyPassword = keystoreProps.getProperty("keyPassword")
-            }
+            storeFile = file("../../upload-keystore.jks")
+            storePassword = "ljc112233"
+            keyAlias = "upload"
+            keyPassword = "ljc112233"
         }
     }
 
     buildTypes {
         getByName("release") {
-            // ✅ KTS 문법: '=' 로 할당
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 }
