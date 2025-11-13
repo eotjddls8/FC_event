bc8340f78e755cee29520867bba7adbce42c34b0
Lee j c 59 <eotjddls902@gmail.com>
2025-11-14 02:40:49 +0900

    1114 최종 업데이트 버전2+ 3.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jonglee.pionevent"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "3.0.0"
     }
 
     compileOptions {
